028e5c45e8bb01df7f12d570d436ed300fdd6ea6
Muhammed <muhammadburhan964@gmail.com>
2021-07-26 00:12:31 +0300

    commit tree 63fe7292436b64e43735517c838422dcd1d40e4f

--- src/components/About.js
+++ src/components/About.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Links from "./Links";
 function About(props) {
+  const checkingBio = props.bio;
+
   return (
     <div id="about">
       <h2>About Me</h2>
-      {props.bio && props.bio.length > 1 ? <p>{props.bio}</p> : null}
+      {checkingBio && checkingBio.length > 1 ? <p>{checkingBio}</p> : null}
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
       {<Links linkedin={props.links.linkedin} github={props.links.github} />}
     </div>
